2455ef909f04905ad3e3e4ea5d67e3a91eeec5a1
azmagainstheworld <zmahdiyyah4@gmail.com>
2025-12-20 21:29:47 +0800

    Fix: delete redundancy in app.js

--- app.js
+++ app.js
@@ -12,27 +12,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    return res.json({
-        msg: "Hello World",
-        subject: "IPPL"
-    });
-});
-
 app.use('/beu004a/users', usersRoute);
 app.use('/beu004a/auth', authRoutes);
 
-const PORT = process.env.PORT || 4000;
-// Tambahkan route ini agar Railway tahu server hidup
-app.get("/", (req, res) => {
-  res.send("Backend J&T Cargo is Running!");
+// Route utama untuk Health Check
+app.get('/', (req, res) => {
+    return res.json({
+        msg: "Backend J&T Cargo is Running",
+        status: "Online"
+    });
 });
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//     console.log("Server is running on http://localhost:" + PORT);
-// });
